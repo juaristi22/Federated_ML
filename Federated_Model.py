6b8e024ed3894cd7c049758e8f50083aedcd0dd6
juaristi22 <juaristi@uni.minerva.edu>
2024-07-22 10:58:19 -0500

    update

--- Federated_Model.py
+++ Federated_Model.py
@@ -54,13 +54,33 @@ class CNNModel(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
         )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=hidden_units * 16 * 16, out_features=output_shape),
+            nn.Linear(in_features=hidden_units * 8 * 8, out_features=output_shape),
         )
 
     def forward(self, x):
         x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
         x = self.classifier(x)
         return x
 
