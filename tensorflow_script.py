92d76b76ac1cbbf1eb68b090705e1dc1f1b868bc
juaristi22 <juaristi@uni.minerva.edu>
2024-07-26 17:52:28 -0500

    update

--- tensorflow_script.py
+++ tensorflow_script.py
@@ -287,7 +287,7 @@ def record_experiments(
             + ".png",
         )
 
-    plot_loss_curves(aggregator_results["Global_Model"], filename=filename, config=experiment_config)
+    plot_loss_curves(accuracy_results, filename=filename, config=experiment_config)
 
 
 def experiment_configs(max_n_models, max_bf=None, max_height=None):
